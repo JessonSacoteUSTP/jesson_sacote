77507e54b0c6bf6e2599f74656bc3c313306e351
jessonsacoteustp <jessonsacote.js@gmail.com>
2020-06-10 14:13:49 +0800

    Final Commit

--- config.php
+++ config.php
@@ -2,9 +2,9 @@
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
 define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'sacote');
-define('DB_PASSWORD', '123456');
-define('DB_NAME', 'demo');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'jesson_sacote_2r3');
  
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
